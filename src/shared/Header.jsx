1b4bd640e8b2a7e2c00072e5a74fa6dfe5830e6f
Manzhak <manzhak.com@gmail.com>
2021-11-26 17:27:47 +0300

    hmr

--- src/shared/Header.jsx
+++ src/shared/Header.jsx
@@ -1,9 +1,13 @@
+import { hot } from 'react-hot-loader/root';
 import React from 'react';
 
-export function Header() {
+function HeaderComponent() {
   return (
     <header>
-      <h1>Header</h1>
+      <h1>Header!</h1>
+      <p>Hello</p>
     </header>
   );
 }
+
+export const Header = hot(HeaderComponent);
